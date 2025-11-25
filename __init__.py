516b2df464f65a7784e178d64eeab1455fe870c3
THIAGO YUKIO DA COSTA KAGOIKI <thiago_kagoiki@estudante.sesisenai.org.br>
2025-11-25 14:12:25 -0300

    Calculo das raizes

--- __init__.py
+++ __init__.py
@@ -2,3 +2,11 @@ import math
 
 def delta(a: float, b: float, c: float):
     return b ** 2 - 4 * a * c
+
+def raizSoma(a: float, b: float, c: float):
+    deltaResult = delta(a, b, c)
+    return (-b + math.sqrt(deltaResult)) / (2 * a)
+
+def raizSubtracao(a: float, b: float, c: float):
+    deltaResult = delta(a, b, c)
+    return (-b - math.sqrt(deltaResult)) / (2 * a)
